d8fd8517af4fccb01c3fca00ef4a5adad1aaffc4
Erixsony <test@test.com>
2022-12-07 22:41:44 +0200

    Adding web driver wait//working list locators/ finding elemnet on page

--- src/test/java/FirstSeleniumTest.java
+++ src/test/java/FirstSeleniumTest.java
@@ -2,6 +2,7 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
@@ -12,9 +13,13 @@ public class FirstSeleniumTest {
     public void openHomePageCheck() {
         System.setProperty("webdriver.chrome.driver", "C://chromedriver.exe");
         WebDriver browser = new ChromeDriver();
+        browser.manage().window().maximize();
         browser.get("http://www.discovercars.com/");
 
         WebDriverWait wait = new WebDriverWait(browser, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.elementToBeSelected(ACCEPT_COOKIES_BTN));
+
+        browser.findElement(ACCEPT_COOKIES_BTN).click();
 
     }
 }
